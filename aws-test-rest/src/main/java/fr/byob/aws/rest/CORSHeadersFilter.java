5b79f28656475c5f530d4bb98646c6b7c2501c84
Pereira Gérald <kojiro.sazaki@gmail.com>
2013-01-18 09:42:54 +0100

    UTF-8

--- aws-test-rest/src/main/java/fr/byob/aws/rest/CORSHeadersFilter.java
+++ aws-test-rest/src/main/java/fr/byob/aws/rest/CORSHeadersFilter.java
@@ -41,7 +41,7 @@ public class CORSHeadersFilter implements Filter {
 			FilterChain chain) throws IOException, ServletException {
 		final HttpServletResponse httpReponse = (HttpServletResponse) response;
 		log.info("Reponse headers added !");
-		httpReponse.setHeader("Access-Control-Allow-Origin", "*"); // TODO limiter à localhost et à l'url de l'appli cliente
+		httpReponse.setHeader("Access-Control-Allow-Origin", "*"); // TODO limiter a localhost et a l'url de l'appli cliente
 		httpReponse.setHeader("Access-Control-Allow-Methods","PUT, GET, POST, DELETE, OPTIONS");
 		httpReponse.setHeader("Access-Control-Allow-Headers",
 				"authorization,content-type");
