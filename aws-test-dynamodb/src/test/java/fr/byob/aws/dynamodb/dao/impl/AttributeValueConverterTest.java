5565f211e8c1a85b5e4b2a8cdce307d50cbd1990
Pereira Gérald <kojiro.sazaki@gmail.com>
2013-01-21 11:38:06 +0100

    test correction

--- aws-test-dynamodb/src/test/java/fr/byob/aws/dynamodb/dao/impl/AttributeValueConverterTest.java
+++ aws-test-dynamodb/src/test/java/fr/byob/aws/dynamodb/dao/impl/AttributeValueConverterTest.java
@@ -27,56 +27,56 @@ public class AttributeValueConverterTest {
 	
 	@Test
 	public void stringToAttributeValueTest(){
-		assertEquals(new AttributeValue(), stringToAttributeValue(null));
+		assertFalse(stringToAttributeValue(null).isPresent());
 		assertEquals("test",stringToAttributeValue("test").get().getS());
 	}
 	
 	@Test
 	public void integerToAttributeValueTest(){
-		assertNull(integerToAttributeValue(null));
+		assertFalse(integerToAttributeValue(null).isPresent());
 		assertEquals("10",integerToAttributeValue(10).get().getN());
 	}
 	
 	@Test
 	public void doubleToAttributeValueTest(){
-		assertNull(doubleToAttributeValue(null));
+		assertFalse(doubleToAttributeValue(null).isPresent());
 		assertEquals("10.0",doubleToAttributeValue(10d).get().getN());
 	}
 	
 	@Test
 	public void stringsToAttributeValueTest(){
-		assertNull(stringsToAttributeValue(null));
+		assertFalse(stringsToAttributeValue(null).isPresent());
 		assertEquals(Arrays.asList("test","test1"),stringsToAttributeValue(Arrays.asList("test","test1")).get().getSS());
 	}
 	
 	@Test
 	public void booleanToAttributeValueTest(){
-		assertNull(booleanToAttributeValue(null));
+		assertFalse(booleanToAttributeValue(null).isPresent());
 		assertEquals("1",booleanToAttributeValue(true).get().getN());
 		assertEquals("0",booleanToAttributeValue(false).get().getN());
 	}
 	
 	@Test
 	public void attributeValueToStringTest(){
-		assertNull(attributeValueToString(null));
-		assertEquals("test",attributeValueToString(new AttributeValue("test")));
+		assertFalse(attributeValueToString(null).isPresent());
+		assertEquals("test",attributeValueToString(new AttributeValue("test")).get());
 	}
 	
 	@Test
 	public void attributeValueToIntegerTest(){
-		assertNull(attributeValueToInteger(null));
-		assertEquals(Integer.valueOf(10),attributeValueToInteger(new AttributeValue().withN("10")));
+		assertFalse(attributeValueToInteger(null).isPresent());
+		assertEquals(Integer.valueOf(10),attributeValueToInteger(new AttributeValue().withN("10")).get());
 	}
 	
 	@Test
 	public void attributeValueToDoubleTest(){
-		assertNull(attributeValueToDouble(null));
-		assertEquals(Double.valueOf(10d),attributeValueToDouble(new AttributeValue().withN("10.0")));
+		assertFalse(attributeValueToDouble(null).isPresent());
+		assertEquals(Double.valueOf(10d),attributeValueToDouble(new AttributeValue().withN("10.0")).get());
 	}
 	
 	@Test
 	public void attributeValueToStringsTest(){
-		assertNull(attributeValueToStrings(null));
+		assertTrue(attributeValueToStrings(null).isEmpty());
 		assertEquals(Arrays.asList("test","test1"),attributeValueToStrings(new AttributeValue(Arrays.asList("test","test1"))));
 	}
 	
